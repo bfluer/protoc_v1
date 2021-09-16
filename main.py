3f6ca2bc33dde1c1db9db964560f1c5df998b197
Bernhard <bernhard.fluer@br-fluer.com>
2021-09-16 14:50:57 +0200

    a

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+NULL_CHAR = chr(0)
+def write_report(report):
+	with open ('/dev/hidg0', 'rb+') as fd:
+		fd.write(report.encode())
+
+while(1):
+    try:
+        write_report(NULL_CHAR)
+    except:
+        continue
+    subprocess.call('sh', '/home/pi/SANWriter/protoc_v1/start.sh')
