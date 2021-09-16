5fc5e0dafc141acb700a891f1432ebec77f9abec
Bernhard <bernhard.fluer@br-fluer.com>
2021-09-16 13:22:46 +0200

    as

--- playground.py
+++ playground.py
@@ -2,9 +2,9 @@
 # -*- encoding: utf-8 -*-
 
 import sys
-import codec
+import codecs
 
-with codec.open (sys.argv[1], 'r', 'UTF-8') as f:
+with codecs.open (sys.argv[1], 'r', 'UTF-8') as f:
     prot = f.read()
     prot.decode()
     for let in prot:
