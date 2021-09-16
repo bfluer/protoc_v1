606ae46572fc0667a1679014ce0427f9fe84f1ef
Bernhard <bernhard.fluer@br-fluer.com>
2021-09-16 13:25:36 +0200

    ..

--- playground.py
+++ playground.py
@@ -6,7 +6,7 @@ import codecs
 
 with codecs.open (sys.argv[1], 'r', 'UTF-8') as f:
     prot = f.read()
-    prot.decode()
+    prot.decode('utf-8')
     for let in prot:
         print let
     f.closed
